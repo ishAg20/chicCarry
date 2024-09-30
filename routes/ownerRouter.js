eee3ae5ba48fd396466401c0531bdf5350bb5f23
ishikaaaagarwal20@gmail.com <ishikaaaagarwal20@gmail.com>
2024-09-30 21:32:23 +0530

    Modify owner model, set up environment variables

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owner-model");
 
 router.get("/", (req, res) => {
   res.send("hey");
 });
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async (req, res) => {
+    let owner = await ownerModel.find();
+    if (owner.length > 0) {
+      return res
+        .status(503)
+        .send("You do not have permission to create new owner");
+    }
+    let { fullname, email, password } = req.body;
+    let createdOwner = await ownerModel.create({
+      fullname,
+      email,
+      password,
+    });
+    res.status(201).send(createdOwner);
+  });
+}
 
 module.exports = router;
