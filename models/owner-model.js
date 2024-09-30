5c14b33ab6299030fa42d27079e393f15b2671cc
ishikaaaagarwal20@gmail.com <ishikaaaagarwal20@gmail.com>
2024-09-30 13:36:38 +0530

    Add routes, modify models

---
+++ models/owner-model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const ownerSchema = mongoose.Schema({
+  fullname: String,
+  email: String,
+  password: String,
+  products: { type: Array, default: [] },
+  gstin: String,
+  profilepic: String,
+});
+
+module.exports = mongoose.model("owner", ownerSchema);
