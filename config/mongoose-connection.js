02cbc821f7bb7a3239fbe47a5da54262457a49ed
ishikaaaagarwal20@gmail.com <ishikaaaagarwal20@gmail.com>
2025-02-08 13:31:46 +0530

    Update mongoose connections

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,10 +1,9 @@
-require("dotenv").config();
 const mongoose = require("mongoose");
 
+const uri =
+  "mongodb+srv://agarwalishika2012:ishika123@cluster0.b80jk.mongodb.net/chicCarry?retryWrites=true&w=majority&appName=Cluster0";
+
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("✅ MongoDB Connected Successfully!"))
   .catch((err) => console.error("❌ MongoDB Connection Error:", err));
